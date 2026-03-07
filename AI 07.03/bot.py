9f41baf5ceb6a6f782be103853c5cc64ed2383e1
gefiehh <vikanika5555@gmail.com>
2026-03-07 11:51:23 +0300

    commit2

---
+++ AI 07.03/bot.py
@@ -0,0 +1,61 @@
+import re
+import string
+from handlers import set_name, handle_greeting, handle_farewell, handle_howareyou, handle_time, handle_weather, handle_addition
+from logger import init_db, get_user, save_user, log_message_to_db
+from patterns import special_patterns, normal_patterns
+from nlp_processor import process_nlp_query
+
+class ChatBot:
+    def __init__(self):
+        self.name = None
+        self.user_id = 1
+        self.special_patterns = special_patterns
+        self.normal_patterns = normal_patterns
+        
+        self.load_user()
+
+    def load_user(self):
+        saved_name = get_user(self.user_id)
+        if saved_name:
+            self.name = saved_name
+
+    def process_message(self, message):
+        original_message = message.lower().strip()
+
+        # NLP анализ
+        nlp_response = process_nlp_query(original_message)
+        if nlp_response:
+            return nlp_response
+
+        for pattern, handler_name in self.special_patterns:
+            match = pattern.search(original_message)
+            if match:
+                handler = globals()[handler_name]
+                return handler(match, self)
+
+        message_without_punct = original_message.translate(
+            str.maketrans('', '', string.punctuation)
+        )
+
+        for pattern, handler_name in self.normal_patterns:
+            match = pattern.search(message_without_punct)
+            if match:
+                handler = globals()[handler_name]
+                return handler(match, self)
+
+        return "Я не понимаю запрос."
+    
+    def run_console_chat(self):
+        while True:
+            user_input = input("\n").strip()
+            
+            response = self.process_message(user_input)
+            print(f"{response}")
+            
+            log_message_to_db(user_input, response, self.name)
+
+
+if __name__ == "__main__":
+    init_db()
+    bot = ChatBot()
+    bot.run_console_chat()
